b4ed3c72c7e37b17bfaed0852bf2e1bdebd0390e
cauchym <detaramewo.iuna@gmail.com>
2018-09-07 13:39:24 +0900

    prac1_5, prac1_6 [p17]

--- ch1/prac1_5/main.go
+++ ch1/prac1_5/main.go
@@ -11,11 +11,11 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.RGBA{0x00, 0x00, 0x00, 1}, color.RGBA{0x00, 0xff, 0x00, 1}}
 
 const (
-	whiteIndex = 0 // パレットの最初の色
-	blackIndex = 1 // パレットの次の色
+	blackIndex = 0 // パレットの最初の色
+	greenIndex = 1 // パレットの次の色
 )
 
 func main() {
@@ -25,25 +25,26 @@ func main() {
 
 func lissajous(out io.Writer) {
 	const (
-		cycles = 5 // 発振器xが完了する周回の回数
-		res = 0.001 // 回転の分解能
-		size = 100
+		cycles  = 5     // 発振器xが完了する周回の回数
+		res     = 0.001 // 回転の分解能
+		size    = 100
 		nframes = 64
-		delay = 8
-		)
+		delay   = 8
+	)
 	freq := rand.Float64() * 3.0
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0
-	for i := 0; i< nframes; i++ {
+	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)}
-	gif.EncodeAll(out, &anim)
+		anim.Image = append(anim.Image, img)
 	}
+	gif.EncodeAll(out, &anim)
+}
